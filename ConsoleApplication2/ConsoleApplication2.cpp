015ff6af51c20c5b6968a2c2543d963c43115321
DILAN0 <artembelykov04@gmail.com>
2022-06-20 22:53:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int num,res;
+    cout << "Введите число: ";
+    cin >> num;
+    if (num < 0) {
+        res = num - 10;
+        cout << "Resulte " << res;
+    }
+    else {
+        res = num + 10;
+        cout <<"Resulte "<< res;
+    }
+
+}
+
